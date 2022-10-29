a071e524ff9c10cb9dcfb26645c826097159082d
AMWA_ <AMWA_@GAMEDEV_WONG>
2022-10-29 13:10:12 -0500

    Agregar archivos de proyecto.

---
+++ Phonebook/Contacts.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook
+{
+    public class Contacts
+    {
+        //Properties
+        private string name;
+        private string lastname;
+        private string phone;
+        private string email;
+
+        //Constructors
+        public Contacts(string name, string lastname, string phone, string email)
+        {
+            this.name = name;
+            this.lastname = lastname;
+            this.phone = phone;
+            this.email = email;
+        }
+
+
+        //Setters
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public void SetLastName(string lastname)
+        {
+            this.lastname = lastname;
+        }
+
+        public void SetPhone(string phone)
+        {
+            this.phone = phone;
+        }
+
+        public void SetEmail(string email)
+        {
+            this.email = email;
+        }
+
+        //Getters
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetLastName()
+        {
+            return lastname;
+        }
+
+        public string GetPhone()
+        {
+            return phone;
+        }
+
+        public string GetEmail()
+        {
+            return email;
+        }
+
+        //Behaviours
+        public override bool Equals(object? obj)
+        {
+            if(obj == null)
+            {
+                return false;
+            }
+
+            if(obj == this)
+            {
+                return true;
+            }
+
+            if(this.GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Contacts other = (Contacts)obj;
+
+            return (this.name.Equals(other.name) && this.lastname.Equals(other.lastname)) && this.phone.Equals(other.phone) && this.email.Equals(other.email);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{1} {2}: {3} {4}", 
+                this.name, this.lastname, this.phone, this.email);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+    }
+}
---
+++ Phonebook/Phonebook.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic; // for List
+using System.IO; // for StreamReader, StreamWriter
+using System.Runtime.CompilerServices;
+
+namespace Phonebook
+{
+    public class Phonebook
+    {
+        private const int LOAD_CONTACTS_FILE = 1;
+        private const int SHOW_CONTACTS = 2;
+        private const int ADD_CONTACTS = 3;
+        private const int EDIT_CONTACTS = 4;
+        private const int DELETE_CONTACTS = 5;
+        private const int MERGE_CONTACTS = 6;
+        private const int SAVE_CONTACTS_FILE = 7;
+        private const int EXIT_PROGRAM = 8;
+
+        List<Contacts> cnts = new List<Contacts>();
+
+        public static void Main(string[] args)
+        {
+            Phonebook phb = new Phonebook();
+            phb.Start();
+        }
+
+        public Phonebook()
+        { 
+        }
+
+        public void Start()
+        {
+            int input;
+            do
+            {
+                do
+                {
+                    ShowMainMenu();
+                    input = GetInput();
+                } while (!IsValidInput(input));
+                ProcessInput(input);
+            } while (input != EXIT_PROGRAM);
+
+
+        }
+
+        public void ShowMainMenu()
+        {
+            Console.WriteLine("Welcome to your phonebook.\n\nChoose an option:");
+            Console.WriteLine($"[{LOAD_CONTACTS_FILE}] Load contacts from a file.");
+            Console.WriteLine($"[{SHOW_CONTACTS}] Show all contacts.");
+            Console.WriteLine($"[{ADD_CONTACTS}] Add a contact.");
+            Console.WriteLine($"[{EDIT_CONTACTS}] Edit an existing contact.");
+            Console.WriteLine($"[{DELETE_CONTACTS}] Delete a contact.");
+            Console.WriteLine($"[{MERGE_CONTACTS}] Merge duplicated contacts.");
+            Console.WriteLine($"[{SAVE_CONTACTS_FILE}] Save all contacts in a file.");
+            Console.WriteLine($"[{EXIT_PROGRAM}] Exit.\n");
+            Console.Write("Input: ");
+        }
+
+        public int GetInput()
+        {
+            try
+            {
+                string inputToStringToInt = Console.ReadLine().Trim();
+                
+                return Convert.ToInt16(inputToStringToInt);
+            }
+            catch(Exception e)
+            {
+                return 0;
+            }
+        }
+
+        public bool IsValidInput(int input)
+        {
+            if (input >= 1 && input <= 8)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input\nPlease enter a number from 1 to 8.");
+                return false;
+            }
+        }
+
+        public void ProcessInput(int x)
+        {
+            if (x == LOAD_CONTACTS_FILE)
+            {
+
+            }
+            else if(x == SHOW_CONTACTS)
+            {
+
+            }
+            else if (x == ADD_CONTACTS)
+            {
+                AddContacts();
+            }
+            else if (x == EDIT_CONTACTS)
+            {
+
+            }
+            else if (x == MERGE_CONTACTS)
+            {
+
+            }
+            else if (x == SAVE_CONTACTS_FILE)
+            {
+
+            }
+        }
+
+        public void LoadContactsFile()
+        {
+
+        }
+
+        public void ShowConctacts()
+        {
+            return ;
+        }
+
+        public void AddContacts()
+        {
+            Console.Clear();
+            Contacts contacts = new Contacts(null,null,null,null);
+
+            Console.SetCursorPosition(4, 2);
+            Console.WriteLine("NAME");
+
+
+            Console.SetCursorPosition(24, 2);
+            Console.WriteLine("LAST NAME");
+
+            Console.SetCursorPosition(44, 2);
+            Console.WriteLine("PHONE");
+
+            Console.SetCursorPosition(64, 2);
+            Console.WriteLine("E-MAIL");
+
+            Console.Write("\n\nEnter the name: ");
+            contacts.SetName(Console.ReadLine());
+            Console.SetCursorPosition(4, 3);
+            Console.WriteLine(contacts.GetName());
+
+            //Menu Refresh            
+
+            Console.Clear();
+
+            Console.SetCursorPosition(4, 2);
+            Console.WriteLine("NAME");
+            Console.SetCursorPosition(4, 3);
+            Console.WriteLine(contacts.GetName());
+
+            Console.SetCursorPosition(24, 2);
+            Console.WriteLine("LAST NAME");
+
+
+            Console.SetCursorPosition(44, 2);
+            Console.WriteLine("PHONE");
+
+            Console.SetCursorPosition(64, 2);
+            Console.WriteLine("E-MAIL");
+
+            Console.Write("\n\nEnter the last name: ");
+            contacts.SetLastName(Console.ReadLine());
+            Console.SetCursorPosition(24, 3);
+            Console.WriteLine(contacts.GetLastName());
+
+            //Menu Refresh
+
+            Console.Clear();
+            
+            Console.SetCursorPosition(4, 2);
+            Console.WriteLine("NAME");
+            Console.SetCursorPosition(4, 3);
+            Console.WriteLine(contacts.GetName());
+
+            Console.SetCursorPosition(24, 2);
+            Console.WriteLine("LAST NAME");
+            Console.SetCursorPosition(24, 3);
+            Console.WriteLine(contacts.GetLastName());
+
+            Console.SetCursorPosition(44, 2);
+            Console.WriteLine("PHONE");
+
+
+            Console.SetCursorPosition(64, 2);
+            Console.WriteLine("E-MAIL");
+
+            Console.Write("\n\nEnter the phone number: ");
+            contacts.SetPhone(Console.ReadLine());
+            Console.SetCursorPosition(44, 3);
+            Console.WriteLine(contacts.GetPhone());
+
+            //Menu Refresh
+
+            Console.Clear();
+            
+            Console.SetCursorPosition(4, 2);
+            Console.WriteLine("NAME");
+            Console.SetCursorPosition(4, 3);
+            Console.WriteLine(contacts.GetName());
+
+            Console.SetCursorPosition(24, 2);
+            Console.WriteLine("LAST NAME");
+            Console.SetCursorPosition(24, 3);
+            Console.WriteLine(contacts.GetLastName());
+
+            Console.SetCursorPosition(44, 2);
+            Console.WriteLine("PHONE");
+            Console.SetCursorPosition(44, 3);
+            Console.WriteLine(contacts.GetPhone());
+
+            Console.SetCursorPosition(64, 2);
+            Console.WriteLine("E-MAIL");
+
+            Console.Write("\n\nEnter the e-mail: ");
+            contacts.SetEmail(Console.ReadLine());
+            Console.SetCursorPosition(64, 3);
+            Console.WriteLine(contacts.GetEmail());
+
+            cnts.Add(contacts);
+
+
+            Console.Clear();
+            Console.WriteLine(cnts.Count); //Checks how many contacts are on the list. REMOVE WHEN EVERYTHING IS FUNCTIONAL.
+        }
+
+        public void EditContacts()
+        {
+
+        }
+
+        public void MergeContacts()
+        {
+
+        }
+
+        public void SaveContactsFile()
+        {
+
+
+        }
+    }
+}
+
+
